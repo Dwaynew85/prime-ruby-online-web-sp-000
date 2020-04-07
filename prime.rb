e79e8348f2d64366cda55aa3adf7cc9a1ab445b1
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-04-07 00:49:43 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,6 @@
 def prime?(num)
   (2..(num - 1)).each do |n|
-    if num % n == 0
-      return true
-    elsif -num % n == 0
+    if num % n == 0 || -num % n == 0
       return true
     end
   end
